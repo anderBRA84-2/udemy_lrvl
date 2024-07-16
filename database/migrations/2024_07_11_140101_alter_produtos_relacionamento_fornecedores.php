bd7c95b0eb8c50d38476fc2368fd3399d73b7c92
anderBRA84-2 <andertjk_84@hotmail.com>
2024-07-16 18:08:48 -0300

    Trigesimo quarto Commit

--- database/migrations/2024_07_11_140101_alter_produtos_relacionamento_fornecedores.php
+++ database/migrations/2024_07_11_140101_alter_produtos_relacionamento_fornecedores.php
@@ -11,7 +11,25 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+
+
+
+        schema::table('products',function(Blueprint $table){
+
+        //criamos um usuario padrao para não apagramos nenhum registro na tabela produtos caso contrario teriamos um erro na aplicacao
+        //se fosse a primeira migrate das tabelas nao era necessario
+
+        DB::table('fornecedors')->insertGetId([
+            'name' => 'SG',
+            'site' => 'sg.com.br',
+            'uf'   =>  'RJ'  ,
+            'email'=>  'sg@email.com.br'
+
+        ]);
+
+            $table->unsignedBigInteger('fornecedor_id')->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedors');
+        });
     }
 
     /**
@@ -20,5 +38,12 @@ return new class extends Migration
     public function down(): void
     {
         //
+        schema::table('products',function(Blueprint $table){
+            $table->dropForeign('products_fornecedor_id')->references('id')->on('fornecedors');
+            $table->dropColumn('fornecedor_id');
+        });
+
+
+
     }
 };
