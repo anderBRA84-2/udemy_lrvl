a1dbb20a29e00c259c26171f6cbb67a0caddbcf3
anderBRA84-2 <andertjk_84@hotmail.com>
2024-07-28 13:03:31 -0300

    quadragesimo quinto commit

---
+++ resources/views/app/pedido/create.blade.php
@@ -0,0 +1,28 @@
+@extends('app.layouts.base')
+
+@section('title','Produtos')
+
+@section('content')
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <p> <h1>Pedido- adicionar</h1> </p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li> <a href="{{ route('pedido.index') }}">Voltar</a></li>
+                <li> <a href="{{ route('pedido.index') }}">Consulta</a></li>
+            </ul>
+        </div>
+
+        <div class="informacao-pagina">
+
+            <div style="width:30%; margin-left: auto; margin-right: auto;">
+                @component('app.pedido._components.form_create_edit',['produtos' => $produtos])
+
+                @endcomponent
+
+            </div>
+        </div>
+    </div>
+@endsection
